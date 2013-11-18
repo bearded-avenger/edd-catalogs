f77d26e2780bd6a9eb9223e6e585fefcdbccf2e6
Bearded Avenger <email@nickhaskins.com>
2013-11-18 09:56:02 -0600

    hah! remove exluded option
    end user won't need this and its fuggin up the count - fix laters

--- inc/data.php
+++ inc/data.php
@@ -22,9 +22,6 @@ if(!function_exists('ba_edd_catalog_data')){
         $data 			= json_decode( $remote['body'],true);
         $total 			= isset($data['products']) ? count($data['products']) : false;
 
-        $opts           = get_option('ba_edd_catalog_settings');
-        $getexcluded 	= isset($opts['exclude']) ? $opts['exclude'] : false;
-
         // action
         do_action('edd_catalog_before');
 
@@ -37,37 +34,32 @@ if(!function_exists('ba_edd_catalog_data')){
 
                     for($i=0; $i<$total; $i++) {
 
-                        $exclude         = $getexcluded == $data['products'][$i]['info']['slug'];
+                        // get some vars ready
+                        $getname       	= isset($data['products'][$i]['info']['title']) ? $data['products'][$i]['info']['title'] : false;
+                        $getprice      	= isset($data['products'][$i]['pricing']['amount']) ? $data['products'][$i]['pricing']['amount'] : false;
+                        $getimg        	= isset($data['products'][$i]['info']['thumbnail']) ? $data['products'][$i]['info']['thumbnail'] : false;
+                        $getlink       	= isset($data['products'][$i]['info']['link']) ? $data['products'][$i]['info']['link'] : false;
+                        $slug          	= isset($data['products'][$i]['info']['slug']) ? $data['products'][$i]['info']['slug'] : false;
 
-                        if ( !in_array($exclude, $data) ):
+                        // get plugin path check if installed
+                        $plugin     	= sprintf('%s/%s.php',$slug,$slug);
+                        $isinstalled   	= is_plugin_active($plugin);
 
-                            // get some vars ready
-                            $getname       	= isset($data['products'][$i]['info']['title']) ? $data['products'][$i]['info']['title'] : false;
-                            $getprice      	= isset($data['products'][$i]['pricing']['amount']) ? $data['products'][$i]['pricing']['amount'] : false;
-                            $getimg        	= isset($data['products'][$i]['info']['thumbnail']) ? $data['products'][$i]['info']['thumbnail'] : false;
-                            $getlink       	= isset($data['products'][$i]['info']['link']) ? $data['products'][$i]['info']['link'] : false;
-                            $slug          	= isset($data['products'][$i]['info']['slug']) ? $data['products'][$i]['info']['slug'] : false;
+                        $image          = true == $isinstalled ? sprintf('<a class="edd-catalog-img-link" target="_blank"><img src="%s"></a>',$getimg) : sprintf('<a class="edd-catalog-img-link" href="%s" target="_blank"><img src="%s"></a>',$getlink,$getimg);
+                        $link           = true == $isinstalled ? sprintf('<a class="edd-catalog-notify installed">installed</a>') : sprintf('<a class="edd-catalog-notify" href="%s">Buy Now %s</a>',$getlink,$getprice);
+                        $installclass   = true == $isinstalled ? 'is-installed' : false;
 
-                            // get plugin path check if installed
-                            $plugin     	= sprintf('%s/%s.php',$slug,$slug);
-                            $isinstalled   	= is_plugin_active($plugin);
+                        // title
+                        $title          = sprintf('<h3 class="edd-catalog-item-title">%s</h3>',$getname);
 
-                            $image          = true == $isinstalled ? sprintf('<a class="edd-catalog-img-link" target="_blank"><img src="%s"></a>',$getimg) : sprintf('<a class="edd-catalog-img-link" href="%s" target="_blank"><img src="%s"></a>',$getlink,$getimg);
-                            $link           = true == $isinstalled ? sprintf('<a class="edd-catalog-notify installed">installed</a>') : sprintf('<a class="edd-catalog-notify" href="%s">Buy Now %s</a>',$getlink,$getprice);
-                            $installclass   = true == $isinstalled ? 'is-installed' : false;
+                        // output
+                        $output        .= sprintf('<div class="col-md-2"><div class="edd-catalog-item %s">%s<div class="edd-catalog-item-inner">%s%s</div></div></div>',$installclass,$title,$image,$link);
 
-                            // title
-                            $title          = sprintf('<h3 class="edd-catalog-item-title">%s</h3>',$getname);
+                        if ( ( 0 == $i % 6 ) && ( $i < $total )) {
 
-                            // output
-                            $output        .= sprintf('<div class="col-md-2"><div class="edd-catalog-item %s">%s<div class="edd-catalog-item-inner">%s%s</div></div></div>',$installclass,$title,$image,$link);
+                            $output .= sprintf('</div><div class="row">');
+                        }
 
-                            if ( ( 0 == $i % 6 ) && ( $i < $total )) {
-
-                                $output .= sprintf('</div><div class="row">');
-                            }
-
-                        endif;
                     }
 
                 do_action('edd_catalog_inside_bottom'); // action
